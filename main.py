d2045d705bba87b934d9182c23629a74c1560c2c
surajit14cse <surajit14@cse.pstu.ac.bd>
2022-01-19 10:49:37 -0800

    Sort the list numerically

--- main.py
+++ main.py
@@ -6,7 +6,10 @@
 thislist = ["cherry","banana","apple","mango"]
 thislist.sort()
 print(thislist)
-
+#Sort the list numerically
+thislist = [100,144,12,11,544,234,254,265,7765]
+thislist.sort()
+print(thislist)
 
 
 
